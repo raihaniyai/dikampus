4f95356a6b2c27906929920e3326fa140f2e4f17
raihaniyai <raihaniyai@gmail.com>
2018-07-12 21:06:36 +0700

    try flex message

--- functions/flex.js
+++ functions/flex.js
@@ -7,7 +7,177 @@ var self = {
     var outputParam = body.result.contexts[0].parameters;
     var warung = outputParam.warung;
 
-    return client.replyMessage(replyToken , );
+    return client.replyMessage(replyToken , {
+    "type": "flex",
+    "altText": "this is a flex message",
+    "contents": {
+      {
+      "type": "bubble",
+      "styles": {
+        "footer": {
+          "separator": true
+        }
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "RECEIPT",
+            "weight": "bold",
+            "color": "#1DB446",
+            "size": "sm"
+          },
+          {
+            "type": "text",
+            "text": "Warung Munjul",
+            "weight": "bold",
+            "size": "xxl",
+            "margin": "md"
+          },
+          {
+            "type": "text",
+            "text": "Jalan Sukabirus, Telkom University",
+            "size": "xs",
+            "color": "#aaaaaa",
+            "wrap": true
+          },
+          {
+            "type": "separator",
+            "margin": "xxl"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "margin": "xxl",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Nasi + Ayam Suwir",
+                    "size": "sm",
+                    "color": "#555555",
+                    "flex": 0
+                  },
+                  {
+                    "type": "text",
+                    "text": "10.000",
+                    "size": "sm",
+                    "color": "#111111",
+                    "align": "end"
+                  }
+                ]
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Susu Milo",
+                    "size": "sm",
+                    "color": "#555555",
+                    "flex": 0
+                  },
+                  {
+                    "type": "text",
+                    "text": "4.000",
+                    "size": "sm",
+                    "color": "#111111",
+                    "align": "end"
+                  }
+                ]
+              },
+              {
+                "type": "separator",
+                "margin": "xxl"
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "margin": "xxl",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "TOTAL",
+                    "size": "sm",
+                    "color": "#555555"
+                  },
+                  {
+                    "type": "text",
+                    "text": "Rp 14.000",
+                    "weight": "bold",
+                    "size": "sm",
+                    "color": "#111111",
+                    "align": "end"
+                  }
+                ]
+              },
+              {
+                "type": "separator",
+                "margin": "xxl"
+              },
+              {
+                "type": "box",
+                "layout": "vertical",
+                "margin": "xxl",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "ALAMAT PENGIRIMAN",
+                    "size": "sm",
+                    "color": "#555555"
+                  },
+                  {
+                    "type": "text",
+                    "margin": "sm",
+                    "text": "Jalan Sukabirus no D38, Gang Mesjid Istiqomah",
+                    "wrap": true,
+                    "size": "xs",
+                    "color": "#111111",
+                    "align": "start"
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "spacer",
+                "size": "xl"
+              },
+              {
+                "type": "button",
+                "style": "primary",
+                "color": "#0B5ED7",
+                "action": {
+                  "type": "uri",
+                  "label": "Order",
+                  "uri": "https://linecorp.com"
+                }
+              },
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "Catatan Tambahan",
+                  "uri": "https://linecorp.com"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    }
+    });
   },
   orderNote: function (replyToken, body, url) {
     var db = bot.database;
@@ -16,174 +186,18 @@ var self = {
     var warung = outputParam.warung;
 
     return client.replyMessage(replyToken , {
-      "type": "flex",
-      "altText": "this is a flex message",
-      "contents": {
-        "type": "bubble",
-        "styles": {
-          "footer": {
-            "separator": true
-          }
-        },
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
+      "type": "template",
+      "altText": "Invoice",
+      "template": {
+          "type": "buttons",
+          "text": "Invoice: " + outputParam.menu[0] + " " + outputParam.jumlah[0] + ", "+ outputParam.note + ", dikirim ke "+outputParam.alamat+", Aku sambungin ke warung "+outputParam.warung+" ya? 👨‍🍳 ",
+          "actions": [
             {
-              "type": "text",
-              "text": "RECEIPT",
-              "weight": "bold",
-              "color": "#0B5ED7",
-              "size": "sm"
-            },
-            {
-              "type": "text",
-              "text": "Warung Munjul",
-              "weight": "bold",
-              "size": "xxl",
-              "margin": "md"
-            },
-            {
-              "type": "text",
-              "text": "Jalan Sukabirus, Telkom University",
-              "size": "xs",
-              "color": "#aaaaaa",
-              "wrap": true
-            },
-            {
-              "type": "separator",
-              "margin": "xxl"
-            },
-            {
-              "type": "box",
-              "layout": "vertical",
-              "margin": "xxl",
-              "spacing": "sm",
-              "contents": [
-                {
-                  "type": "box",
-                  "layout": "horizontal",
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "Nasi + Ayam Suwir",
-                      "size": "sm",
-                      "color": "#555555",
-                      "flex": 0
-                    },
-                    {
-                      "type": "text",
-                      "text": "10.000",
-                      "size": "sm",
-                      "color": "#111111",
-                      "align": "end"
-                    }
-                  ]
-                },
-                {
-                  "type": "box",
-                  "layout": "horizontal",
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "Susu Milo",
-                      "size": "sm",
-                      "color": "#555555",
-                      "flex": 0
-                    },
-                    {
-                      "type": "text",
-                      "text": "4.000",
-                      "size": "sm",
-                      "color": "#111111",
-                      "align": "end"
-                    }
-                  ]
-                },
-                {
-                  "type": "separator",
-                  "margin": "xxl"
-                },
-                {
-                  "type": "box",
-                  "layout": "horizontal",
-                  "margin": "xxl",
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "TOTAL",
-                      "size": "sm",
-                      "color": "#555555"
-                    },
-                    {
-                      "type": "text",
-                      "text": "Rp 14.000",
-                      "weight": "bold",
-                      "size": "sm",
-                      "color": "#111111",
-                      "align": "end"
-                    }
-                  ]
-                },
-                {
-                  "type": "separator",
-                  "margin": "xxl"
-                },
-                {
-                  "type": "box",
-                  "layout": "vertical",
-                  "margin": "xxl",
-                  "contents": [
-                    {
-                      "type": "text",
-                      "text": "ALAMAT PENGIRIMAN",
-                      "size": "sm",
-                      "color": "#555555"
-                    },
-                    {
-                      "type": "text",
-                      "margin": "sm",
-                      "text": "Jalan Sukabirus no D38, Gang Mesjid Istiqomah",
-                      "wrap": true,
-                      "size": "xs",
-                      "color": "#111111",
-                      "align": "start"
-                    }
-                  ]
-                }
-              ]
-            },
-            {
-              "type": "separator",
-              "margin": "xxl"
-            },
-            {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "button",
-                  "style": "primary",
-                  "color": "#0B5ED7",
-                  "action": {
-                    "type": "uri",
-                    "label": "Order",
-                    "uri": "https://linecorp.com"
-                  }
-                },
-                {
-                  "type": "button",
-                  "style": "link",
-                  "action": {
-                    "type": "uri",
-                    "label": "Catatan Tambahan",
-                    "uri": "https://linecorp.com"
-                  }
-                }
-              ]
+              "type": "uri",
+              "label": "Iya ✔️",
+              "uri": url
             }
           ]
-        }
       }
     });
   },
