e21f1bb9376de93073a557b0f88655d50d290cec
raihaniyai <raihaniyai@gmail.com>
2018-08-02 14:49:08 +0700

    Fix Crash

--- functions/dialogflow.js
+++ functions/dialogflow.js
@@ -225,10 +225,12 @@ var self = {
               if (!snapshot.val()) {
                 historyRef.push(parameters.alamat);
               }
-              // Sending invoice to user
-              return flex.order(replyToken, idTransaksi, dataWarung);
               process.exit();
+            }, function (errorObject) {
+              console.log("The read failed: " + errorObject.code);
             });
+            // Sending invoice to user
+            return flex.order(replyToken, idTransaksi, dataWarung);
             process.exit();
           });
         });
