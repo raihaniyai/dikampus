283246b5cff901cf08cddf2146ddc2ea6bee057b
raihaniyai <raihaniyai@gmail.com>
2018-08-06 14:00:17 +0700

    Add Condition

--- functions/flex.js
+++ functions/flex.js
@@ -718,168 +718,169 @@ var self = {
             if (warung == warungs) isFound = true;
             if (isFound) res[warung] = dataWarung;
           } else {
-            res[warung]= dataWarung;
+            res[warung] = dataWarung;
           }
           var itemWarung = res[warung];
-          console.log(warung + ": " +itemWarung);
-          if (jmlWarung > 9) {
-            var lainnya = {
-              "type": "bubble",
-              "body": {
-                "type": "box",
-                "layout": "vertical",
-                "spacing": "sm",
-                "action": {
-                  "type":"postback",
-                  "label":"Warung Lainnya",
-                  "data":"data=warung&warung="+warung
-                },
-                "contents": [
-                  {
-                    "type": "button",
-                    "flex": 1,
-                    "gravity": "center",
-                    "action": {
-                      "type":"postback",
-                      "label":"Warung Lainnya",
-                      "data":"data=warung&warung="+warung
-                    }
-                  }
-                ]
-              }
-            };
-            flexMsg.contents.contents.push(lainnya);
-            throw BreakException;
-          }
-          var flexWarung = {
-            "type": "bubble",
-            "hero": {
-              "type": "image",
-              "url": itemWarung.thumbnail,
-              "size": "full",
-              "aspectRatio": "1:1",
-              "aspectMode": "cover",
-              "action": {
-                "type": "message",
-                "text": warung
-              }
-            },
-            "body": {
-              "type": "box",
-              "layout": "vertical",
-              "action": {
-                "type": "message",
-                "text": warung
-              },
-              "contents": [
-                {
-                  "type": "text",
-                  "text": warung,
-                  "weight": "bold",
-                  "size": "xl"
-                },
-                {
+          if (isFound) {
+            if (jmlWarung > 9) {
+              var lainnya = {
+                "type": "bubble",
+                "body": {
                   "type": "box",
                   "layout": "vertical",
-                  "margin": "lg",
                   "spacing": "sm",
+                  "action": {
+                    "type":"postback",
+                    "label":"Warung Lainnya",
+                    "data":"data=warung&warung="+warung
+                  },
                   "contents": [
                     {
-                      "type": "box",
-                      "layout": "baseline",
-                      "spacing": "sm",
-                      "contents": [
-                        {
-                          "type": "text",
-                          "text": "Alamat",
-                          "color": "#aaaaaa",
-                          "size": "sm",
-                          "flex": 1
-                        },
-                        {
-                          "type": "text",
-                          "text": itemWarung.alamat,
-                          "wrap": true,
-                          "color": "#666666",
-                          "size": "sm",
-                          "flex": 4
-                        }
-                      ]
-                    },
-                    {
-                      "type": "box",
-                      "layout": "baseline",
-                      "spacing": "sm",
-                      "contents": [
-                        {
-                          "type": "text",
-                          "text": "Buka",
-                          "color": "#aaaaaa",
-                          "size": "sm",
-                          "flex": 1
-                        },
-                        {
-                          "type": "text",
-                          "text": itemWarung.jamBuka,
-                          "wrap": true,
-                          "color": "#666666",
-                          "size": "sm",
-                          "flex": 4
-                        }
-                      ]
-                    },
-                    {
-                      "type": "box",
-                      "layout": "baseline",
-                      "spacing": "sm",
-                      "contents": [
-                        {
-                          "type": "text",
-                          "text": "Ongkir",
-                          "color": "#aaaaaa",
-                          "size": "sm",
-                          "flex": 1
-                        },
-                        {
-                          "type": "text",
-                          "text": itemWarung.ongkir,
-                          "wrap": true,
-                          "color": "#666666",
-                          "size": "sm",
-                          "flex": 4
-                        }
-                      ]
+                      "type": "button",
+                      "flex": 1,
+                      "gravity": "center",
+                      "action": {
+                        "type":"postback",
+                        "label":"Warung Lainnya",
+                        "data":"data=warung&warung="+warung
+                      }
                     }
                   ]
                 }
-              ]
-            },
-            "footer": {
-              "type": "box",
-              "layout": "vertical",
-              "spacing": "sm",
-              "contents": [
-                {
-                  "type": "button",
-                  "style": "link",
-                  "color": "#0B5ED7",
-                  "height": "sm",
-                  "action": {
-                    "type": "message",
-                    "label": "Pilih Warung",
-                    "text": warung
-                  }
-                },
-                {
-                  "type": "spacer",
-                  "size": "sm"
-                }
-              ],
-              "flex": 0
+              };
+              flexMsg.contents.contents.push(lainnya);
+              throw BreakException;
             }
+            var flexWarung = {
+              "type": "bubble",
+              "hero": {
+                "type": "image",
+                "url": itemWarung.thumbnail,
+                "size": "full",
+                "aspectRatio": "1:1",
+                "aspectMode": "cover",
+                "action": {
+                  "type": "message",
+                  "text": warung
+                }
+              },
+              "body": {
+                "type": "box",
+                "layout": "vertical",
+                "action": {
+                  "type": "message",
+                  "text": warung
+                },
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": warung,
+                    "weight": "bold",
+                    "size": "xl"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "vertical",
+                    "margin": "lg",
+                    "spacing": "sm",
+                    "contents": [
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "Alamat",
+                            "color": "#aaaaaa",
+                            "size": "sm",
+                            "flex": 1
+                          },
+                          {
+                            "type": "text",
+                            "text": itemWarung.alamat,
+                            "wrap": true,
+                            "color": "#666666",
+                            "size": "sm",
+                            "flex": 4
+                          }
+                        ]
+                      },
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "Buka",
+                            "color": "#aaaaaa",
+                            "size": "sm",
+                            "flex": 1
+                          },
+                          {
+                            "type": "text",
+                            "text": itemWarung.jamBuka,
+                            "wrap": true,
+                            "color": "#666666",
+                            "size": "sm",
+                            "flex": 4
+                          }
+                        ]
+                      },
+                      {
+                        "type": "box",
+                        "layout": "baseline",
+                        "spacing": "sm",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": "Ongkir",
+                            "color": "#aaaaaa",
+                            "size": "sm",
+                            "flex": 1
+                          },
+                          {
+                            "type": "text",
+                            "text": itemWarung.ongkir,
+                            "wrap": true,
+                            "color": "#666666",
+                            "size": "sm",
+                            "flex": 4
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              },
+              "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "button",
+                    "style": "link",
+                    "color": "#0B5ED7",
+                    "height": "sm",
+                    "action": {
+                      "type": "message",
+                      "label": "Pilih Warung",
+                      "text": warung
+                    }
+                  },
+                  {
+                    "type": "spacer",
+                    "size": "sm"
+                  }
+                ],
+                "flex": 0
+              }
+            }
+            flexMsg.contents.contents.push(flexWarung);
+            jmlWarung++;
           }
-          jmlWarung++;
-          flexMsg.contents.contents.push(flexWarung);
         });
       } catch (e) {
         if (e !== BreakException) throw e;
