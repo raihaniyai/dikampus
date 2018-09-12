0675d0a22034294d920fb668703faa2566c31e73
Muhammad Husain <husentod10@gmail.com>
2018-09-12 15:14:51 +0700

    transaksi

---
+++ functions/analytics.js
@@ -0,0 +1,100 @@
+// kalau bot, pakai ini
+const bot = require('./../bot.js');
+
+// kalau local pakai ini
+// const firebase = require("firebase-admin");
+// var serviceAccount = require("./../serviceAccount.json");
+// firebase.initializeApp({
+//     credential: firebase.credential.cert(serviceAccount),
+//     databaseURL: "https://tesbor-733b7.firebaseio.com"
+//   });
+// const db = firebase.database();
+
+var self = {
+    saveTransaction : function(idTransaksi, data){
+        const db = bot.database;
+        var refTransaksi = db.ref("warung/"+data.warung+"/transaksi/"+ idTransaksi);
+        var total = 0;
+        
+        for(var key in data.pesanan){
+            if(data.pesanan.hasOwnProperty(key)){
+                total += data.pesanan[key].harga * data.pesanan[key].jumlah;
+            }
+        }
+        
+        var data_transaksi = {
+            'item' : data.pesanan,
+            'total_price' : total,
+            'user' : data.user,
+            'waktu' : data.waktu,
+        };
+        console.log(data);
+        var tes = refTransaksi.set(data_transaksi, function(error){
+            if(error){
+                console.log("Error: "+error);
+            }else{
+                console.log("Transaction Saved")
+            }
+        });
+    },
+
+    getTotalDeliveryAllShop: function(idTransaksi){
+        var ref = db.ref("transaksi/makanan/");
+        ref.once("value", function(snapshot){
+            var item = new Object();
+            // instantiate hashmap
+            snapshot.forEach(function(child){
+                var data = child.val();
+                if(typeof data.pesanan != "undefined"){
+                    item[data.warung] = new Object();
+                    var date = String(data.waktu).split(" ")[0];
+                    item[data.warung][date] = new Object();
+                    item[data.warung][date]['total_delivery'] = 0;
+                    item[data.warung][date]['total_item_terjual'] = 0;
+                    item[data.warung][date]['total_transaksi_idr'] = 0;
+                }
+            });
+            snapshot.forEach(function(child){
+                var data = child.val();
+                var date = String(data.waktu).split(" ")[0];
+                if(typeof data.pesanan != "undefined"){
+                    if(typeof item[data.warung][date] != "undefined")
+                    item[data.warung][date]['total_delivery'] += 1; 
+                }
+            });
+            console.log(item);
+            
+        })
+    },
+    tes: function(idTransaksi){
+        var ref = db.ref("transaksi/makanan/");
+        ref.once("value", function(snapshot){
+            snapshot.forEach(function(child){
+                var data = child.val();
+                if(String(child.key).includes("-LLmesb-InP5NW_3vjvY")){
+                    console.log(data);
+                }
+            });
+        })
+    },
+    tes2: function(){
+        console.log("Saving Transaction");
+        var ref = db.ref("warung/"+'data.warung'+"/transaksi/"+ 'idTransaksi');
+        var data_transaksi = {
+            'item' : 'data.pesanan',
+            'total_price' : 'data.totalHarga',
+            'user' : 'data.user',
+            'waktu' : 'data.waktu',
+        };
+        ref.push(data_transaksi, function(error){
+            if(error){
+                console.log("Error: "+error);
+            }else{
+                console.log("Transaction Saved")
+            }
+        });
+    }
+    
+};
+
+module.exports = self;
--- functions/flex.js
+++ functions/flex.js
@@ -1,4 +1,5 @@
 const bot = require('./../bot.js');
+const analytics = require('./analytics.js');
 
 var self = {
   order: function (replyToken, idTransaksi, dataWarung) {
@@ -16,6 +17,7 @@ var self = {
       for (var menu in data.pesanan) {
         text += menu + " (" + data.pesanan[menu].jumlah + "), "
       }
+      analytics.saveTransaction(idTransaksi, data);
       text += "kirim ke " + data.alamat;
       text = encodeURIComponent(text);
       var url = "https://api.whatsapp.com/send?phone="+dataWarung.nomorWarung+"&text="+text;
