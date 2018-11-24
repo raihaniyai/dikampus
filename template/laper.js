37fa7dfa1e7e2e6401426ba055ac4bf06f7b2bac
raihaniyai <raihaniyai@gmail.com>
2018-11-25 04:00:04 +0700

    asdfasdfwer

---
+++ template/laper.js
@@ -0,0 +1,42 @@
+const bot = require('./../bot.js');
+
+var self = {
+  imagemap: function () {
+    flex = {
+      "type": "imagemap",
+      "baseUrl": "https://dikosan.herokuapp.com/images/imagemap_laper",
+      "altText": "Laper",
+      "baseSize": {
+        "width": 1040,
+        "height": 1040
+      },
+      "actions": [
+        {
+          "type": "uri",
+          "linkUri": "https://instagram.com",
+          "area": {
+            "x": 0,
+            "y": 0,
+            "width": 1040,
+            "height": 1040
+          }
+        }
+      ],
+      "quickReply": {
+        "items": [
+          {
+            "type": "action",
+            "action": {
+              "type": "message",
+              "label": "Menu Serina",
+              "text": "Menu"
+            }
+          }
+        ]
+      }
+    };
+    return flex;
+  }
+};
+
+module.exports = self;
