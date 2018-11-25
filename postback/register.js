cfc144e8d3c6b53ee82a0b27039fd35d8dbf20e1
raihaniyai <raihaniyai@gmail.com>
2018-11-26 01:32:03 +0700

    hehe

--- postback/register.js
+++ postback/register.js
@@ -9,7 +9,7 @@ var self = {
     var type = res.type;
     var db = bot.database;
     if (res.action == 'jurusan') {
-      return register.main(null, replyToken, source.userId, session, res);
+      return register.main(null, replyToken, userId, session, res);
     } else if (res.action == 'done') {
       store.set(userId, {status: null})
     }
