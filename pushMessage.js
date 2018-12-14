b4c4bcaafd3da0035d8b984e7081ee2276dfbbe0
raihaniyai <raihaniyai@gmail.com>
2018-12-15 01:23:20 +0700

    change message

--- pushMessage.js
+++ pushMessage.js
@@ -3,16 +3,28 @@ const store = require('store2')
 
 module.exports = {
   main: function (req, res) {
-    console.log("msuksini");
-    console.log(req.body);
     var replyText = bot.replyText;
     var client = bot.client;
     var type = res.type;
     var db = bot.database;
-    const message = {
-      type: 'text',
-      text: 'Hello World!'
-    };
+    if (req.body.status == 'diproses') {
+      answer = ['Pesanan kakak sedang diproses', 'lagi diproses nih', 'okayy, pesanan kakak diproses']
+      var message = {
+        type: 'text',
+        text: answer[Math.floor(Math.random()*answer.length)]
+      }
+    } else if (req.body.status == 'diantar') {
+      answer = ['Pesanannya otw kak', 'Otewee', 'Otw ya kak']
+      var message = {
+        type: 'text',
+        text: answer[Math.floor(Math.random()*answer.length)]
+      }
+    } else {
+      var message = {
+        type: 'text',
+        text: "answer[Math.floor(Math.random()*answer.length)]"
+      }
+    }
 
     return client.pushMessage(req.body.userId, message)
       // .then(() => {
