bd14d3b40f8290c57ea08b7b54e1bb1c6bfc15a2
raihaniyai <raihaniyai@gmail.com>
2018-12-15 00:05:32 +0700

    Add some file

---
+++ pushMessage.js
@@ -0,0 +1,26 @@
+const bot = require('./../bot.js')
+const store = require('store2')
+
+var module.exports = {
+  main: function (req, res) {
+    var replyText = bot.replyText;
+    var client = bot.client;
+    var type = res.type;
+    var db = bot.database;
+    const message = {
+      type: 'text',
+      text: 'Hello World!'
+    };
+
+    return client.pushMessage(req.body.userId, message)
+      // .then(() => {
+      //   ...
+      // })
+      // .catch((err) => {
+      //   console.log("Error");
+      // });
+    res.send({
+        success: "ok"
+    });
+  }
+};
