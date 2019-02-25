ec92931aa805b0da4bfdd0cd431ee0ee33c9ad3f
Raihan Iyai <raihaniyai@gmail.com>
2019-02-26 02:20:39 +0700

    test notif

--- bot.js
+++ bot.js
@@ -66,7 +66,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.post('/push', (req, res) => {
   console.log(req.body);
-  return pushMessage.main(req, res)
+  return pushMessage.notif(req, res)
 });
 
 // Simple Reply Function
